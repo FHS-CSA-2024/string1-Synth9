d6c84858b0c90636e4d506534a3d07dc95ed8c01
Synth9 <96803186+Synth9@users.noreply.github.com>
2024-08-27 22:28:08 -0700

    Update String1.java
    17/17 complete?

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -295,7 +295,18 @@ public class String1
      * deFront("away") â†’ "aay"
      */
     public String deFront(String str) {    
-        return unimplemented;
+        if (str.charAt(0) == 'a' && str.charAt(1) == 'b'){
+            return str;
+        }
+        else if (str.charAt(0) == 'a'){
+            return str.substring(0,1) + str.substring(2);
+        }
+        else if (str.charAt(0) == 'b'){
+            return str.substring(1);
+        }
+        else{
+            return str.substring(2);
+        }
     }
 
 }
